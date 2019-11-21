280057962572e47f1594e76496fb6710e30eddbe
Biniam <biniamt23@yahoo.com>
2019-11-20 22:43:50 -0800

    user story complete, just need to add image url

---
+++ ECom/ECom/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECom.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECom.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IInventory _context;
+
+        public ProductController(IInventory product)
+        {
+            _context = product;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ECom/ECom/Models/Interfaces/IInventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECom.Models.Interfaces
+{
+    public interface IInventory 
+    {
+        //Create product 
+        Task CreateProductAsync(Product product);
+
+        //Get individual product
+        Task<Product> GetProductByIDAsync(int id);
+
+        //Get all products
+        Task<List<Product>> GetProductsAsync();
+
+        //Update specific product
+        Task UpdateProductAsync(Product product);
+
+        //Delete specific product
+        Task DeleteProductAsync(int id);
+    }
+}
