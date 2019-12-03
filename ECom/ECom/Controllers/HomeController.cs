f4d61b8a65e48ee714238725e4f300dc2d8c6664
Alejandroid101 <kik.rdl3@gmail.com>
2019-12-03 10:54:29 -0800

    advancements in cart implementation, not finished though.

--- ECom/ECom/Controllers/HomeController.cs
+++ ECom/ECom/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
 
 namespace ECom.Controllers
 {
@@ -10,6 +12,9 @@ namespace ECom.Controllers
     
     public class HomeController : Controller
     {
+        private IEmailSender _email;
+
+        public HomeController(IEmailSender email)
         public IActionResult Index()
         {
             return View();
